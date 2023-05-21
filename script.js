f8af49bfc2676a461a600af3a91d275d6ba22b06
jnorrie <jillian.business@outlook.com>
2023-05-21 16:24:46 +1000

    Changed Style of Map

--- script.js
+++ script.js
@@ -26,15 +26,21 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// //Map Style
+
+L.tileLayer('https://tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token=befJwIMQ1LGqatCnABKXIGIx5p9gItfQXB5wnRuxHVOnyrHE47IQlGF9Opri8EqU', {}).addTo(map);
+map.attributionControl.addAttribution("<a href=\"https://www.jawg.io\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors")
+
+
 //Minimum Zoom
 map.options.minZoom = 7;
 
 //Setting Bounds
 
 const somersetLatLng = L.latLng( -27.1190, 152.5511);
-const jacobsWellLatLng = L.latLng(-27.786249, 153.359535);
+const coralSeaLatLng = L.latLng(-27.863158, 153.572523);
 
-const bounds = L.latLngBounds(somersetLatLng, jacobsWellLatLng);
+const bounds = L.latLngBounds(somersetLatLng, coralSeaLatLng);
 map.maxBoundsViscosity = 1.0;
 map.setMaxBounds(bounds);
 
